12deeb5a0a650ab7c5e76ef93232f56df956c846
Joseph Brimeyer <54119655+josephbrimeyer@users.noreply.github.com>
2022-03-09 20:21:18 -0800

    added state and effect for abouts.

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,29 +2,27 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 import {images} from '../../constants'
-
 import './About.scss';
+import { urlFor, client} from '../../client';
 
-const abouts = [
-  {title: 'Web Development', description: "I am a good web developer.", imgUrl: images.about01 
+// const abouts = [
+//   {title: 'Web Development', description: "I am a good web developer.", imgUrl: images.about01},
+//   {title: 'Front End Development', description: "I am a good web developer.", imgUrl: images.about02},
+//   {title: 'Back End Development', description: "I am a good web developer.", imgUrl: images.about03},
+//   {title: 'MERN Stack', description: "I am a good web developer.", imgUrl: images.about04}
+// ];
 
-},
-{
-  title: 'Web Design', description: "I am a good web developer.", imgUrl: images.about02 
-
-},{
-  title: 'Front End', description: "I am a good web developer.", imgUrl: images.about03 
-
-},
-{
-  title: 'Back End', description: "I am a good web developer.", imgUrl: images.about04 
-
-}
-];
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
   return (
     <>
-<h2 className="head-text"> I Know that <span>Good Design</span> <br /> means <span>Good Business</span> 
+<h2 className="head-text"> I Know that <span>Good Apps</span> <br /> means <span>Good Business</span> 
 
 </h2>
 <div className="app__profiles">{abouts.map((about, index) => (
